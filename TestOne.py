d81521e224644a2c5fc8fb8fc1bb13b354a17854
Nithesh <nitesh.ankoshe@gmail.com>
2025-07-21 16:47:47 +0200

    data types changes

--- TestOne.py
+++ TestOne.py
@@ -1,2 +1,17 @@
-print("Hello Python and Git")
-print("File is added successfully")
+#Data type
+testString = "this is a string data type"
+print(testString)
+
+testInt = 10
+print(testInt)
+
+testFloat = 22.3
+print(testFloat)
+
+testComplex = 2 + 3j
+print(testComplex)
+
+testBool = False
+print(testBool)
+
+#keep adding as I come across
