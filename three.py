da9fa7ba87d0458171e84c326701965bb8492d2a
niteshankoshe <nitesh.ankoshe@gmail.com>
2025-07-22 21:10:28 +0200

    added more function

--- three.py
+++ three.py
@@ -3,3 +3,13 @@ def printFun():
     print("Print from the function")
 
 printFun()
+
+#calculate the number and return from the function
+def summarize() -> int:
+    sumUp = number1 + number2
+    return sumUp
+
+number1 = int(input("Enter the number 1\n"))
+number2 = int(input("enter the second number\n"))
+#sum_num = summarize()
+print("total is", summarize())
