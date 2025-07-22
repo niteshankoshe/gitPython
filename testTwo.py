eb0910ef21600632dabeb585ae7cc5322230e09d
niteshankoshe <nitesh.ankoshe@gmail.com>
2025-07-22 20:58:45 +0200

    match added

--- testTwo.py
+++ testTwo.py
@@ -19,3 +19,12 @@ elif(const_number == input_number2):
 else:
     print("Try Later!!")
 
+#Matched
+input_number3 = int(input("Enter number between 1 to 5\n"))
+match input_number3:
+    case 1: print("Your number is 1")
+    case 2: print("your number is 2")
+    case 3|4: print("your number is 3 or 4")
+    case 5: print("your number is 5")
+    case _: print("you entered someting out of range")
+
