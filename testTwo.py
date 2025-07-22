5102b6d2fc3e2b543c32e773558832ea4da56f14
niteshankoshe <nitesh.ankoshe@gmail.com>
2025-07-22 20:49:36 +0200

    elif added

--- testTwo.py
+++ testTwo.py
@@ -4,3 +4,18 @@ if(user_number % 2 == 0):
     print("Even Number")
 else:
     print("Odd number")
+
+#elif
+const_number = 50
+input_number = int(input("Enter the input number\n"))
+input_number1 = int(input("Another number choice\n"))
+input_number2 = int(input("Enter last choice:\n"))
+if(const_number == input_number):
+    print("you got the match first time")
+elif(const_number == input_number1):
+    print("Second matched")
+elif(const_number == input_number2):
+    print("Last choice is matched")
+else:
+    print("Try Later!!")
+
